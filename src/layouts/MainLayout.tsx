ccb19ede072316186656505155a22af15371076b
USERDAV <dbezberdij@gmail.com>
2025-06-25 16:00:29 +0300

    hw6

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
+import { MenuComponent } from "../menu/MenuComponent"
 
 
 export function MainLayout() {
     return (
         <>
+        <MenuComponent/>
         <Outlet/>
         </>
     )
